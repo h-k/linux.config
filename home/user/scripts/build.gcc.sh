c36c56712acb962ea50af9e29d8d4738c393a124
Alexander Savchenko <oleksandr.savchenko@globallogic.com>
2014-06-10 11:52:48 +0300

    build.gcc.sh: Add system notify
    Change-Id: Iec2c89a520e67c6542b6641727aa8b61422086d2
    Signed-off-by: Alexander Savchenko <oleksandr.savchenko@globallogic.com>

--- home/user/scripts/build.gcc.sh
+++ home/user/scripts/build.gcc.sh
@@ -4,8 +4,7 @@ ROOTPWD=`pwd`
 TARGET=arm-eabi
 PREFIX=$ROOTPWD/$TARGET-build
 HOST=`gcc -dumpmachine`
-LEVEL=4
-
+THREADS=2
 
 unset TARGET_GCC_OPTIONS
 TARGET_GCC_OPTIONS="$TARGET_GCC_OPTIONS --prefix=$PREFIX/$TARGET"
@@ -43,12 +42,12 @@ EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-fpu=vfp"
 EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-arch=armv5te"
 EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --enable-target-optspace"
 EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-abi=aapcs"
-EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-gcc-version=4.8.1"
-EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-binutils-version=2.23.2"
-EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-gmp-version=5.1.2"
+EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-gcc-version=4.9.0 2014 06 10"
+EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-binutils-version=2.24"
+EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-gmp-version=6.0.0a"
 EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-mpfr-version=3.1.2"
-EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-mpc-version=1.0.1"
-EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-gdb-version=7.6"
+EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-mpc-version=1.0.2"
+EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-gdb-version=7.7"
 EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --with-sysroot=$PREFIX/sysroot"
 EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --enable-gold"
 EXTRA_GCC_OPTIONS="$EXTRA_GCC_OPTIONS --disable-gold"
@@ -89,42 +88,42 @@ fi
 echo "STAGE 1: gmp"
 prepare gmp
 ../configure --prefix=$PREFIX/gmp --disable-shared --enable-static
-make -j $LEVEL ; make install
+make -j $THREADS ; make install
 
 echo "STAGE 2: mpfr"
 prepare mpfr
 ../configure --prefix=$PREFIX/mpfr --with-gmp=$PREFIX/gmp --disable-shared --enable-static
-make -j $LEVEL ; make install
+make -j $THREADS ; make install
 
 echo "STAGE 3: mpc"
 prepare mpc
 ../configure --prefix=$PREFIX/mpc --with-gmp=$PREFIX/gmp --with-mpfr=$PREFIX/mpfr --disable-shared --enable-static
-make -j $LEVEL ; make install
+make -j $THREADS ; make install
 
 echo "STAGE 4: binutils"
 prepare binutils
 ../configure --prefix=$PREFIX/$TARGET --target=$TARGET --enable-interwork --disable-multilib --disable-nls --disable-shared --enable-threads=posix --with-gcc --with-gnu-as --with-gnu-ld --enable-gold
-make -j $LEVEL ; make install
+make -j $THREADS ; make install
 
-echo "STAGE 5: gcc"
+echo "STAGE 5: gcc --without-headers"
 prepare gcc
 ../configure $TARGET_GCC_OPTIONS $COMMON_GCC_OPTIONS $REQUIRED_GCC_OPTIONS $EXTRA_GCC_OPTIONS --without-headers
-make all-gcc -j $LEVEL ; make install-gcc
+make all-gcc -j $THREADS ; make install-gcc
 
 echo "STAGE 6: newlib"
 prepare newlib
 ../configure --prefix=$PREFIX/$TARGET --target=$TARGET --enable-interwork --disable-multilib --with-gnu-as --with-gnu-ld --disable-nls
-make -j $LEVEL ; make install
+make -j $THREADS ; make install
 
 echo "STAGE 7: gcc"
 prepare gcc
 ../configure $TARGET_GCC_OPTIONS $COMMON_GCC_OPTIONS $REQUIRED_GCC_OPTIONS $EXTRA_GCC_OPTIONS
-make -j $LEVEL ; make install
+make -j $THREADS ; make install
 
 echo "STAGE 8: gdb"
 prepare gdb
 ../configure --prefix=$PREFIX/$TARGET --target=$TARGET --disable-nls --with-gmp=$PREFIX/gmp --with-mpfr=$PREFIX/mpfr --with-mpc=$PREFIX/mpc --with-libexpat
-make -j $LEVEL ; make install
+make -j $THREADS ; make install
 
 #finish
 cd $ROOTPWD
@@ -136,3 +135,5 @@ echo "| (_| | (_) | | | |  __/ "
 echo " \__,_|\___/|_| |_|\___| "
 echo "                         "
 echo "                         "
+
+/usr/bin/notify-send "gcc 4.9.0 build done!"
