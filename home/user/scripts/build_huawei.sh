987c3cfedcf9c9ec71aa2888dac2dc858b0a7d0b
Alexander Savchenko <oleksandr.savchenko@celeno.com>
2020-02-11 10:57:35 +0200

    update docs
    Change-Id: I7ec0406a64f4f5cfa4532a05e8d4f2a841fb70e9

--- home/user/scripts/build_huawei.sh
+++ home/user/scripts/build_huawei.sh
@@ -17,16 +17,15 @@ rm -rf celeno_clr_package_*/  SOURCE_CODE_*.tar.bz || true
 tar xf SOURCE_CODE_celeno_clr_package_cl2400_*
 cd celeno_clr_package_cl2400_Huawei/
 
-#export PATH=/export/dev-root/huawei_sdk/opt/RTOS/207.5.0/arm32A9le_4.4_ek_micro/gnu/bin/:$PATH
 export PATH=/export/dev-root/huawei_sdk/SDK_Kernel4.4.185/opt/RTOS/207.5.0/arm32A9le_4.4_ek_micro/gnu/bin/:$PATH
 sed -i -e 's#^\s*DEF_CONF_CROSS_COMPILE.*$#DEF_CONF_CROSS_COMPILE = /home/alexander/ccache/huawei/arm-linux-musleabi-#' src/celeno.mk
-#export CCACHE_PATH=/export/dev-root/huawei_sdk/opt/RTOS/207.5.0/arm32A9le_4.4_ek_micro/gnu/bin:$PATH
 export CCACHE_PATH=/export/dev-root/huawei_sdk/SDK_Kernel4.4.185/opt/RTOS/207.5.0/arm32A9le_4.4_ek_micro/gnu/bin:$PATH
 
 make
 
-mv build cl2400
-tar -czf cl2400.tar.bz2 cl2400
+#mv build cl2400
+#tar -czf cl2400.tar.bz2 cl2400
+tar -czf cl2400.tar.bz2 build
 
 cp cl2400.tar.bz2 /tftpboot/
 
